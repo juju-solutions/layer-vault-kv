b059d6c847db0fee159320f8bfb8101b280e0b67
Adam Dyess <adam.dyess@canonical.com>
2021-11-09 08:30:43 -0600

    ensure leader_set is only called on changes to the leader's hashes, not on EVERY exit

--- reactive/vault_kv.py
+++ reactive/vault_kv.py
@@ -53,10 +53,10 @@ def manage_app_kv_flags():
 def update_app_kv_hashes():
     try:
         app_kv = vault_kv.VaultAppKV()
-        if hookenv.is_leader():
-            # force hooks to run on non-leader units
-            hookenv.leader_set({"vault-kv-nonce": host.pwgen(8)})
         if app_kv.any_changed():
+            if hookenv.is_leader():
+                # force hooks to run on non-leader units
+                hookenv.leader_set({"vault-kv-nonce": host.pwgen(8)})
             # Update the local unit hashes at successful exit
             app_kv.update_hashes()
     except vault_kv.VaultNotReady:
--- tests/unit/test_reactive.py
+++ tests/unit/test_reactive.py
@@ -1,8 +1,9 @@
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 
 import pytest
 
 from reactive.vault_kv import update_app_kv_hashes
+from charms.layer.vault_kv import VaultAppKV
 
 
 @pytest.fixture()
@@ -19,9 +20,20 @@ def mock_vault_config():
         yield vc
 
 
+@pytest.fixture()
+def destroy_vault_kv():
+    """Teardown singleton instance created in each unit test."""
+    yield
+    del VaultAppKV._singleton_instance
+
+
 @patch("hvac.Client", autospec=True)
-@patch("charmhelpers.core.hookenv.local_unit", return_value="unit-test/0")
-def test_update_app_kv_hashes(_mock_local_unit, mock_hvac_client, mock_vault_config):
+@patch("charmhelpers.core.hookenv.local_unit", Mock(return_value="unit-test/0"))
+@patch("charmhelpers.core.hookenv.is_leader", Mock(return_value=True))
+@patch("charmhelpers.core.hookenv.leader_set")
+def test_update_app_kv_hashes_leader(
+    mock_leader_set, mock_hvac_client, mock_vault_config, destroy_vault_kv
+):
     def mock_read(path):
         if path == "charm-unit-test/kv/app":
             return dict(data={"tested-key": "tested-value"})
@@ -30,8 +42,58 @@ def test_update_app_kv_hashes(_mock_local_unit, mock_hvac_client, mock_vault_con
 
     client = mock_hvac_client.return_value
     client.read.side_effect = mock_read
+
+    # --------------------------------
+    # exit #1
+    # Both leader_set and update_hashes should be executed
     update_app_kv_hashes()
     client.write.assert_called_once_with(
         "charm-unit-test/kv/app-hashes/0",
         **{"tested-key": "b40d0066377d3ec7015ab9f498699940"}
     )
+    mock_leader_set.assert_called_once()
+
+    # --------------------------------
+    # exit #2
+    # Neither leader_set nor update_hashes should be executed
+    mock_leader_set.reset_mock()
+    client.write.reset_mock()
+    update_app_kv_hashes()
+    client.write.assert_not_called()
+    mock_leader_set.assert_not_called()
+
+
+@patch("hvac.Client", autospec=True)
+@patch("charmhelpers.core.hookenv.local_unit", Mock(return_value="unit-test/0"))
+@patch("charmhelpers.core.hookenv.is_leader", Mock(return_value=False))
+@patch("charmhelpers.core.hookenv.leader_set")
+def test_update_app_kv_hashes_follower(
+    mock_leader_set, mock_hvac_client, mock_vault_config, destroy_vault_kv
+):
+    def mock_read(path):
+        if path == "charm-unit-test/kv/app":
+            return dict(data={"tested-key": "tested-value"})
+        elif path == "charm-unit-test/kv/app-hashes/0":
+            return {}
+
+    client = mock_hvac_client.return_value
+    client.read.side_effect = mock_read
+
+    # --------------------------------
+    # exit #1
+    # Only update_hashes should be executed, not leader_set
+    update_app_kv_hashes()
+    client.write.assert_called_once_with(
+        "charm-unit-test/kv/app-hashes/0",
+        **{"tested-key": "b40d0066377d3ec7015ab9f498699940"}
+    )
+    mock_leader_set.assert_not_called()
+
+    # --------------------------------
+    # exit #2
+    # Neither leader_set nor update_hashes should be executed
+    mock_leader_set.reset_mock()
+    client.write.reset_mock()
+    update_app_kv_hashes()
+    client.write.assert_not_called()
+    mock_leader_set.assert_not_called()
